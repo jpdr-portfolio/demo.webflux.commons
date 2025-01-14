77fc2ce7046d8c5161ef15d3f3f88d1030a46306
jpdr.apps@gmail.com <jpdr.apps@gmail.com>
2025-01-14 19:04:55 -0300

    excluded double dots from name

--- src/main/java/com/jpdr/apps/demo/webflux/commons/validation/InputValidator.java
+++ src/main/java/com/jpdr/apps/demo/webflux/commons/validation/InputValidator.java
@@ -3,13 +3,18 @@ package com.jpdr.apps.demo.webflux.commons.validation;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 
+import java.math.BigDecimal;
+import java.util.Set;
 import java.util.regex.Pattern;
 
 @Slf4j
 public class InputValidator {
   
-  private static final Pattern NAME_PATTERN = Pattern.compile("^[^\\[\\]\\%\\/\\=\\:\\^\\<\\>\\#\\|]+$");
+  private InputValidator(){}
+  
+  private static final Pattern NAME_PATTERN = Pattern.compile("^[^\\[\\]\\%\\/\\=\\^\\<\\>\\#\\|]+$");
   private static final Pattern EMAIL_PATTERN = Pattern.compile("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$");
+  private static final Set<String> GENDERS = Set.of("Female", "Male", "Other");
   
   public static boolean isValidName(String name){
     if(StringUtils.isBlank(name) || !NAME_PATTERN.matcher(name).matches()){
@@ -22,6 +27,22 @@ public class InputValidator {
   public static boolean isValidEmail(String email){
     if(StringUtils.isBlank(email) || !EMAIL_PATTERN.matcher(email).matches()){
       log.warn("Invalid email: {}",email);
+      return false;
+    }
+    return true;
+  }
+  
+  public static boolean isValidAmount(BigDecimal amount){
+    if(amount.compareTo(BigDecimal.ZERO) < 1){
+      log.warn("Invalid amount: {}",amount.toPlainString());
+      return false;
+    }
+    return true;
+  }
+  
+  public static boolean isValidGender(String gender){
+    if(!GENDERS.contains(gender)){
+      log.warn("Invalid gender: {}", gender);
       return false;
     }
     return true;
